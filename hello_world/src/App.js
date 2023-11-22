4262e8cb7b323e706d2df3aaa2e7b3b59368d1b4
Arunkumar1102 <arunpanjugula011@gmail.com>
2023-11-22 14:20:39 +0530

    register file

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,28 +1,36 @@
-import React from 'react';
-
-
+ import React, {useState} from 'react'
+import axios from 'axios';
+ 
 const App = () => {
-  return <>
-    <Content />
-    <Content />
-    <Content />
-    <Content />
-  </>
-}
-
-const Content= () => {
-  return <article>
-     <ImageComponents />;
-  <Title />;
-  </article>
-}
-
-
-const ImageComponents = () => {
-  return <img src="https://img10.hotstar.com/image/upload/f_auto,q_90,w_256/sources/r1/cms/prod/7724/1617724-v-aaaa754e258c" alt="vikram"/>
-}
-
-const Title = () => {
-  return<h3>Vikram</h3>
-}
-export default App;
+  const [data, setData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    conformPassword: '',
+  })  
+  const { username, email, password, conformPassword } = data;
+  const changeHandler = e => {
+    setData({...data , [e.target.name]:[e.target.value]})
+  }
+  const submitHandler = e => {
+    e.preventDefault();
+    axios.post('https://arunkumar1102-bd257-default-rtdb.firebaseio.com/register.json',
+    data).then(()=> alert("submitted successfully"))     
+  }
+   return (
+     <div>
+       <center>
+         <form onSubmit={submitHandler}>
+           <input type='text' name='username' value={username} placeholder='username' onChange={changeHandler}/> <br/>
+           <input type='email' name='email' value={email} placeholder='email'onChange={changeHandler}/> <br/>
+           <input type='password' name='password' value={password} placeholder='password'onChange={changeHandler}/> <br/>
+           <input type='password' name='conformPassword' value={conformPassword} placeholder='conformpassword'onChange={changeHandler}/> <br/>
+           <input type='submit' name='submit' /> 
+         </form>
+       </center>
+     </div>
+   )
+ }
+ 
+ export default App
+ 
